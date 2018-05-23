85c88c4a00a30a564128a1e92004b4d2a28453c9
pgogtt <pgogtt@gmail.com>
2018-05-23 22:54:56 +0900

    Initial Commit

---
+++ FirtstKadaiApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirtstKadaiApp
+//
+//  Created by yoshi on 2018/05/23.
+//  Copyright © 2018 tttyoshittt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
